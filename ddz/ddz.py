38ad443248b4f08eddea477abca92273c553f919
YuJun <skyduy@outlook.com>
2020-02-09 09:11:50 +0800

    更改python环境

--- ddz/ddz.py
+++ ddz/ddz.py
@@ -1,12 +1,13 @@
-import cext
 from pprint import pprint
+import cext  # type: ignore
 
-handcards = [0,0,0,0,0,1,1,3,3,3,3,3,1,1]
-all_cards = [4] * 13 + [1,1]
-no_cards = [0] * 15
+HANDCARDS = [0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 3, 3, 1, 1]
+ALL_CARDS = [4] * 13 + [1, 1]
+NO_CARDS = [0] * 15
+LAST_CARDS = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0]
 
-print(len(cext.get_actions(all_cards, no_cards)))
-print(len(cext.get_actions(handcards, no_cards)))
-
-last_cards = [1,0,0,0,0,0,0,0,0,0,3,0,0,0,0]
-pprint(cext.get_actions(handcards, last_cards))
+print(len(cext.get_actions(ALL_CARDS, NO_CARDS)))
+print('--')
+print(len(cext.get_actions(HANDCARDS, NO_CARDS)))
+print('--')
+pprint(cext.get_actions(HANDCARDS, LAST_CARDS))
