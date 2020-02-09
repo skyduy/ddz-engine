cdefcb3a6d205bc808e4b788ca402fa9d3740a60
YuJun <skyduy@outlook.com>
2020-02-09 20:02:02 +0800

    f**k visual studio

--- README.md
+++
@@ -1,3 +0,0 @@
-# ddz
-
-unfinished: test module
--- cextText/pch.h
+++ cextText/pch.h
@@ -4,5 +4,14 @@
 //
 
 #pragma once
+#include<string>
+#include<vector>
 
 #include "gtest/gtest.h"
+
+using std::string;
+using std::vector;
+
+string value2Card(int v);
+bool print(vector<int>& card);
+bool print(vector<vector<int>>& card);
